6e2de0b1931e6782f386f567b285c235c0fc3ab1
Maxence Gollier <mgollier45@gmail.com>
2024-11-22 16:03:15 -0500

    first version of golub-riley

--- src/utils.jl
+++ src/utils.jl
@@ -23,3 +23,46 @@ function qrm_refine!(spmat :: qrm_spmat{T}, spfct :: qrm_spfct{T}, x :: Abstract
   qrm_solve!(spfct, y, Δx, transp = 'n')
   @. x = x + Δx
 end
+
+#Given an underdetermined, rank defficient system Ax = b, compute the least-norm solution with Golub-Riley iteration
+# TODO: add something for rank defficient least squares as well (i.e compute (Aᵀ)†z)
+function qrm_golub_riley!(spmat :: qrm_spmat{T},  xₖ :: AbstractVector{T} , b :: AbstractVector{T}, bₖ :: AbstractVector{T}, Δx ::AbstractVector{T}; α :: T = eps(T)^(1//4), max_iter ::Int = 10, tol ::T = T(1e-3)) where {T <: Real}
+  
+  n_A = spmat.mat.n - spmat.mat.m
+  m = spmat.mat.m
+  nz = spmat.mat.nz
+  n = spmat.mat.n - spmat.mat.m # The matrix A is (m,n), spmat represents a (m,n+m) matrix
+
+  @assert n > 0
+  @assert length(b) == spmat.mat.m
+  @assert length(xₖ) == spmat.mat.n
+  @assert length(bₖ) == spmat.mat.m
+  @assert length(Δx) == spmat.mat.n
+  @assert all(spmat.irn[i + nz - m] == Cint(i) for i = 1:m )
+  @assert all(spmat.jcn[i + nz - m] == Cint(i+n) for i = 1:m)
+  spmat.val[nz-m + 1:nz] .= α
+
+  spfct = qrm_spfct_init(spmat)
+  qrm_set(spfct, "qrm_keeph", 0)
+  qrm_analyse!(spmat, spfct, transp ='t')
+  qrm_factorize!(spmat, spfct, transp = 't')
+
+  k = 0
+  solved = false
+  xₖ .= T(0)
+  while k < max_iter && !solved
+
+    qrm_spmat_mv!(spmat, T(1), xₖ, T(0), bₖ)
+    @. bₖ = b - bₖ
+
+    qrm_solve!(spfct, bₖ, Δx, transp = 't')
+    qrm_solve!(spfct, Δx, bₖ, transp = 'n')
+
+    qrm_spmat_mv!(spmat, T(1), bₖ, T(0), Δx, transp = 't')
+
+    @. xₖ = xₖ + Δx
+    
+    solved = norm(Δx) ≤ tol*norm(xₖ)
+    k = k + 1
+  end
+end
